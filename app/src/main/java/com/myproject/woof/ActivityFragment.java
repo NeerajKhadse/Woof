bf5094b9141cacd74b6617a18d01dc696b4d9126
Neeraj Khadse <neerajkhadse1999@gmail.com>
2021-06-27 15:47:59 +0530

    Added Geofencing

--- app/src/main/java/com/myproject/woof/ActivityFragment.java
+++ app/src/main/java/com/myproject/woof/ActivityFragment.java
@@ -1,5 +1,7 @@
 package com.myproject.woof;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,13 +9,113 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
-public class ActivityFragment extends Fragment {
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ActivityFragment extends Fragment {
+    private static final int MAX_X_VALUE = 7;
+    private static final int MAX_Y_VALUE = 20;
+    private static final int MIN_Y_VALUE = 1;
+    private static final String SET_LABEL = "Avg Steps";
+    private static final String[] DAYS = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+
+    private BarChart chart;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private BarData createChartData() {
+        ArrayList<BarEntry> values = new ArrayList<>();
+        for (int i = 0; i < MAX_X_VALUE; i++) {
+            float x = i;
+            float y = ThreadLocalRandom.current().nextInt(MIN_Y_VALUE, MAX_Y_VALUE);
+            values.add(new BarEntry(x, y));
+        }
+
+        BarDataSet set1 = new BarDataSet(values, SET_LABEL);
+        set1.setColors(
+                Color.rgb(0,0,0),
+                Color.rgb(232, 232, 232),
+                Color.rgb(0,0,0),
+                Color.rgb(232, 232, 232),
+                Color.rgb(0,0,0),
+                Color.rgb(232, 232, 232),
+                Color.rgb(0,0,0)
+        );
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        BarData data = new BarData(dataSets);
+
+        return data;
+    }
+
+    private void prepareChartData(BarData data) {
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.rgb(0,0,0));
+        chart.setData(data);
+        chart.invalidate();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_activity, container, false);
+        View view = inflater.inflate(R.layout.fragment_activity, container, false);
+
+        chart = view.findViewById(R.id.fragment_verticalbarchart_chart);
+
+        BarData data = createChartData();
+        configureChartAppearance();
+        prepareChartData(data);
+
+        return view;
+    }
+
+    private void configureChartAppearance() {
+        chart.getDescription().setEnabled(false);
+        chart.setDrawValueAboveBar(true);
+        chart.getXAxis().setDrawGridLines(false);
+        chart.getAxisLeft().setDrawGridLines(false);
+        chart.getAxisRight().setDrawGridLines(false);
+        chart.getAxisLeft().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+        chart.getLegend().setEnabled(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setTextSize(12f);
+        xAxis.setTextColor(Color.rgb(0,0,0));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return DAYS[(int) value];
+            }
+        });
+
+        YAxis axisLeft = chart.getAxisLeft();
+        axisLeft.setGranularity(10f);
+        axisLeft.setAxisMinimum(0);
+
+        YAxis axisRight = chart.getAxisRight();
+        axisRight.setGranularity(10f);
+        axisRight.setAxisMinimum(0);
     }
 }
